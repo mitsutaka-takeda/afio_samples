fd16cd71b30d74c9ca22970d737bccb5c1361119
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2015-06-08 17:45:20 +0900

    Change the directory structure and split a common function into a separate file.

--- src/listing_files_in_a_directory.cpp
+++ src/listing_files_in_a_directory.cpp
@@ -1,30 +1,9 @@
-#include <future>
 #include <vector>
 #include <boost/afio.hpp>
 
-// Adding then capability to std::future.
-template <typename T, typename Work>
-auto get_work_done(std::future<T> &f, Work &w)-> decltype(w(f.get())) {
-    return w(f.get());
-}
+#include "async/future_extension.hpp"
 
-template <typename Work>
-auto get_work_done(std::future<void> &f, Work &w)-> decltype(w()) {
-    f.wait();
-    return w();
-}
-
-template <typename T, typename Work>
-auto then(std::future<T> f, Work w) -> std::future<decltype(w(f.get()))> {
-    return std::async([](std::future<T> f, Work w) {
-            return get_work_done(f,w);
-        },
-        std::move(f),
-        std::move(w));
-}
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     if(argc != 2){
         std::cout << "You need to specify a file path at the command line." << std::endl;
         return 1;
@@ -39,7 +18,7 @@ int main(int argc, char *argv[])
     auto enumerate = async_file_io_dispatcher->enumerate(
         boost::afio::async_enumerate_op_req(
             dir));
-    then(std::move(enumerate.first),
+    async::then(std::move(enumerate.first),
         [](const auto f){
             for(auto&& entry : f.first){
                 std::cout << entry.name() << std::endl;
